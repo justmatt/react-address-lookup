938033b75337526e9b29e2500ed20dee2759ed23
Matt Campbell <mwc0000@gmail.com>
2020-04-26 16:55:30 +0100

    Support for postcodeonly querystring parameter

--- src/postcode_lookup/PostcodeLookup.js
+++ src/postcode_lookup/PostcodeLookup.js
@@ -16,13 +16,13 @@ export default class PostcodeLookup extends Component {
 	};
 
 	search = e => {
-		let { apiKey, identifier, country } = this.props;
+		let { apiKey, identifier, country, postcodeOnly } = this.props;
 		country === "" ? (country = "UK") : (country = country); // defaultProps only works if the prop is absent, not set to an empty string
 		this.setState({ loading: true });
 		fetch(
 			`https://ws.postcoder.com/pcw/${apiKey}/address/${country}/${encodeURIComponent(
 				this.state.input
-			)}?format=json&lines=2&identifier=${identifier}`
+			)}?format=json&lines=2&identifier=${identifier}&postcodeonly=${postcodeOnly}`
 		)
 			.then(response => {
 				return response.json();
@@ -66,9 +66,11 @@ export default class PostcodeLookup extends Component {
 PostcodeLookup.defaultProps = {
 	apiKey: "PCW45-12345-12345-1234X",
 	identifer: "react-address-lookup",
-	country: "UK",
+  country: "UK",
+  postcodeOnly: false,
 };
 PostcodeLookup.propTypes = {
 	apiKey: PropTypes.string,
-	placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  placeholder: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
+  postcodeOnly: PropTypes.bool
 };
